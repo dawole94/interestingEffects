346cce845ee2af510b0a73987153989cbd00667b
Dawid <dawid.oleksy94@gmail.com>
2024-10-15 06:14:34 +0200

    backgrount gradient animation simpler code

--- src/app/components/Background.jsx
+++ src/app/components/Background.jsx
@@ -7,33 +7,21 @@ const Background = () => {
   const elementRef = useRef(null);
 
   useEffect(() => {
-    const tl = gsap.timeline({ repeat: -1, yoyo: true });
-
-    tl.to(elementRef.current, {
+    gsap.to(elementRef.current, {
       duration: 5,
       background: "linear-gradient(45deg, #0000ff, #ff0000)",
-      // backgroundSize: "300% 300%",
-      backgroundPosition: "0% 0%",
-      ease: "linear"
-    })
-    .to(elementRef.current, {
-      duration: 0,
-      backgroundPosition: "100% 100%",
       ease: "linear",
-      // background: "linear-gradient(45deg, #0000ff, #ff0000)",
-      // backgroundSize: "300% 300%",
-    });
-
+      yoyo: true,
+      repeat: -1
+    })
   }, []);
 
   return (
     <div
       ref={elementRef}
       style={{
-        // width: "100%",
         height: "100%",
         background: "linear-gradient(45deg, #ff0000, #0000ff)",
-        backgroundSize: "300% 300%"
       }}
     />
   );
